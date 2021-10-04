9c0e87b168c6eafe3693ba6b38c5e413c76e6ad4
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2021-10-04 10:11:21 +0200

    Drafted first version of todos.service.ts

--- Source/Svc.Todos.Angular/src/app/services/todos/todo.model.ts
+++
@@ -1,6 +0,0 @@
-export interface Todo {
-  id: string;
-  title: string;
-  description: string;
-  isCompleted: boolean;
-}
--- Source/Svc.Todos.Angular/src/app/services/todos/todos.service.ts
+++ Source/Svc.Todos.Angular/src/app/services/todos/todos.service.ts
@@ -1,33 +1,63 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { ID } from '@datorama/akita';
 import { tap } from 'rxjs/operators';
-import { Todo } from './todo.model';
 import { TodosStore } from './todos.store';
+import { environment } from "../../../environments/environment";
+import {
+  CreateTodoCommand,
+  CreateTodoResult,
+  DeleteTodoCommand,
+  GetTodoCommand,
+  GetTodoResult,
+  GetTodosCommand,
+  GetTodosResult,
+  TodoDto,
+  UpdateTodoCommand,
+  UpdateTodoResult
+} from "../../models/data-contracts";
+import { Observable } from "rxjs";
 
-@Injectable({ providedIn: 'root' })
+@Injectable({providedIn: 'root'})
 export class TodosService {
+
+  private baseUrl = `${environment.todoApi}/todos`;
 
   constructor(private todosStore: TodosStore, private http: HttpClient) {
   }
 
-
-  get() {
-    return this.http.get<Todo[]>('https://api.com').pipe(tap(entities => {
-      this.todosStore.set(entities);
-    }));
+  getTodos(request: GetTodosCommand): Observable<GetTodosResult> {
+    const url = `${this.baseUrl}/get-todos`;
+    return this.http.post<GetTodosResult>(url, request).pipe(
+      tap(res => this.todosStore.set(res.todos as TodoDto[]))
+    );
   }
 
-  add(todo: Todo) {
-    this.todosStore.add(todo);
+  public getTodo(request: GetTodoCommand): Observable<GetTodoResult> {
+    const url = `${this.baseUrl}/get-todo`;
+    return this.http.post<GetTodoResult>(url, request).pipe(
+      tap(res => this.todosStore.upsert(request.todoId, res.todo as TodoDto))
+    );
   }
 
-  update(id, todo: Partial<Todo>) {
-    this.todosStore.update(id, todo);
+  public createTodo(request: CreateTodoCommand): Observable<CreateTodoResult> {
+    const url = `${this.baseUrl}/create-todo`;
+    return this.http.post<CreateTodoResult>(url, request).pipe(
+      tap(res => this.todosStore.upsert(res.createdTodo?.id, res.createdTodo as TodoDto))
+    );
   }
 
-  remove(id: ID) {
-    this.todosStore.remove(id);
+  public updateTodo(request: UpdateTodoCommand): Observable<UpdateTodoResult> {
+    const url = `${this.baseUrl}/update-todo`;
+    return this.http.post<UpdateTodoResult>(url, request).pipe(
+      tap(res => this.todosStore.upsert(res.updatedTodo?.id, res.updatedTodo as TodoDto))
+    );
+  }
+
+  public deleteTodo(request: DeleteTodoCommand): Observable<void> {
+    const url = `${this.baseUrl}/delete-todo`;
+    return this.http.post<void>(url, request).pipe(
+      tap(() => this.todosStore.remove(request.todoId))
+    );
   }
 
 }
