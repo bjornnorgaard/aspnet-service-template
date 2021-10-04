4429f5dde5e44ca68ea99672248354bb624746b6
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2021-10-04 08:56:54 +0200

    Added akita with 'ng add @datorama/akita'

--- Source/Svc.Todos.Angular/src/app/app.module.ts
+++ Source/Svc.Todos.Angular/src/app/app.module.ts
@@ -10,6 +10,9 @@ import en from '@angular/common/locales/en';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AkitaNgDevtools } from '@datorama/akita-ngdevtools';
+import { AkitaNgRouterStoreModule } from '@datorama/akita-ng-router-store';
+import { environment } from '../environments/environment';
 
 registerLocaleData(en);
 
@@ -22,9 +25,12 @@ registerLocaleData(en);
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    environment.production ? [] : AkitaNgDevtools.forRoot(),
+    AkitaNgRouterStoreModule
   ],
-  providers: [{ provide: NZ_I18N, useValue: en_US }],
+  providers: [{provide: NZ_I18N, useValue: en_US}],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
