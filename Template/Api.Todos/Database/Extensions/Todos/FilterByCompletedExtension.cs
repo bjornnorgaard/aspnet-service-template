79055abaaac29f8d0be224f7807d57cf856a568a
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2022-01-15 14:08:56 +0100

    Added FilterByCompletedExtension.cs for showing shared queries

---
+++ Template/Api.Todos/Database/Extensions/Todos/FilterByCompletedExtension.cs
@@ -0,0 +1,11 @@
+﻿using Api.Todos.Database.Models;
+
+namespace Api.Todos.Database.Extensions.Todos;
+
+public static class FilterByCompletedExtension
+{
+    public static IQueryable<Todo> WhereCompleted(this IQueryable<Todo> q)
+    {
+        return q.Where(todo => todo.IsCompleted == true);
+    }
+}
