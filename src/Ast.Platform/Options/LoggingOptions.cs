b93638f1bb83698617e83a5d57e3ceda6a3b2b79
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2024-05-10 14:42:26 +0200

    Removed currently redundant observability services from compose. Most of these will likely be reintroducted later, for now we're just simplifying the setup

--- src/Ast.Platform/Options/LoggingOptions.cs
+++ src/Ast.Platform/Options/LoggingOptions.cs
@@ -5,7 +5,6 @@ namespace Ast.Platform.Options;
 public class LoggingOptions : AbstractOptions
 {
     public string ApplicationName { get; set; }
-    public string ElasticSearchUrl { get; set; }
 
     public LoggingOptions(IConfiguration configuration) : base(configuration)
     {
